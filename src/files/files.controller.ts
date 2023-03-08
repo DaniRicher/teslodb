7639a2eea5fc25fc92fdf31d72b92ad83c600017
Daniel Richerson <richerdani3@gmail.com>
2023-03-08 00:56:50 -0500

    FileSystem

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,15 +1,26 @@
-import { Controller, Post } from '@nestjs/common';
-import { UploadedFile, UseInterceptors } from '@nestjs/common/decorators';
+import { Controller, Post, Get } from '@nestjs/common';
+import { Param, Res, UploadedFile, UseInterceptors } from '@nestjs/common/decorators';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { FilesService } from './files.service';
 import { BadRequestException } from '@nestjs/common/exceptions';
 import { diskStorage } from 'multer';
 import { fileFilter, fileNamer } from './helpers';
+import { Response } from 'express';
 
 @Controller('files')
 export class FilesController {
 
   constructor(private readonly filesService: FilesService) {}
+
+  @Get('product/:imageName')
+  findProductImage( 
+      @Res() res: Response,
+      @Param('imageName') imageName: string 
+    ) {
+    const path = this.filesService.getStaticProductImage( imageName );
+    
+    res.sendFile( path );
+  }
 
   @Post('product')
   @UseInterceptors( FileInterceptor('file', {
@@ -28,10 +39,11 @@ export class FilesController {
       throw new BadRequestException('Make a sure that the file is an image');
     }
 
-    console.log(file);
+    // const secureUrl = `${ file.filename }`;
+    const secureUrl = `http://localhost:3000/api/files/product/8980de73-315a-4e7c-9b09-516ecf279229.png`;
 
     return {
-      filename: file.originalname
+      secureUrl
     };
   }
 }
